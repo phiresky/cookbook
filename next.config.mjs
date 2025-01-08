db070fd2d1de65a8847ae79dac860844d68b7015
phiresky <phireskyde+git@gmail.com>
2025-01-08 23:34:44 +0100

    gh actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: {unoptimized: true}
+    images: {unoptimized: true},
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
